297a843fd7a1d79b862605d8357803558cb00f48
Alexandra Somyai <somyai.alexandra@yahoo.com>
2020-10-08 14:21:36 +0300

    Modificare Somyai Alexandra

---
+++ SomyaiAlexandra/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SomyaiAlexandra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Salut, sunt Alexandra");
+            Console.WriteLine("Introduceti inaltimea");
+
+            int inaltime;
+            try
+            {
+                inaltime = int.Parse(Console.ReadLine());
+                Console.WriteLine($"Aveti {inaltime} inaltime");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Va rugam sa folositi doar numere intregi!");
+                return;
+            }
+
+
+
+        }
+    }
+}
