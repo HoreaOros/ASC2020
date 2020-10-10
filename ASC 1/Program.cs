feab4d1958f66416ea7cf1435adb56df2f609abb
Covasan Iosif <covasan.iosif7@gmail.com>
2020-10-10 18:25:50 +0300

    Numarpantof

--- ASC 1/Program.cs
+++ ASC 1/Program.cs
@@ -11,6 +11,22 @@ namespace ASC_1
         static void Main(string[] args)
         {
             Console.WriteLine("Helloo world of C#!!");
+            Console.WriteLine("Introduceti numarul de la pantof:");
+            string line;
+            line = Console.ReadLine();
+            int numarPantof;
+            try 
+	{	        
+		numarPantof = int.Parse(line);
+                Console.WriteLine($"Aveti {numarPantof} numarul la pantof");
+	}
+	catch (FormatException e)
+	{
+
+		Console.WriteLine(e.Message);
+	}
+
+            
         }
     }
 }
