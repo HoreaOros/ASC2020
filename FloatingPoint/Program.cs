2d3109d4c26b8e37cb17f39bc71af066c7bd6fd6
Horea Oros <horea.oros@gmail.com>
2020-11-06 09:20:20 +0200

    add FloatingPoint, GUID project

---
+++ FloatingPoint/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatingPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float f = .0314f;
+            double d = 3.14;
+
+            Console.WriteLine($"{f:F3}{Environment.NewLine}{d:E}");
+
+            // TODO
+            // Se cere sa se afiseze secventa binara corespunzatoare unei valori 
+            // de tip float respectiv double. 
+            // probabil ar trebui sa folositi operatori pe biti &,|,^,<<,>>,~ 
+
+        }
+    }
+}
---
+++ GUID/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GUID
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Guid g = Guid.NewGuid();
+            foreach (var item in g.ToByteArray())
+            {
+                Console.Write($"{item,3:X2} ");
+            }
+
+            Console.WriteLine();
+            foreach (var item in g.ToByteArray())
+            {
+                Console.Write($"{item,3} ");
+            }
+
+        }
+    }
+}
